7570a777225c9c01ff2822311d0d25a45a9042fc
MoniSandy0204 <hackersandy02@gmail.com>
2023-10-27 11:26:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ LOAD = getenv("LOAD", "").split()
 
 NO_LOAD = getenv("NO_LOAD", "").split()
 
-TOKEN = getenv("TOKEN", "6577602955:AAEF_60boG9BWXWadlKctzZEp4WYHKcnat8")
+TOKEN = getenv("TOKEN", "6577602955:AAHpVhSSau9jV7K1fPFVDa2io5F7eMfRlqk")
 
 MONGO_DB_URL = getenv(
     "MONGO_DB_URL",
