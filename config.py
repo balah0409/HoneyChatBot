480140ce4e807eefbce21a9b730e712f0c32f9a6
MoniSandy0204 <hackersandy02@gmail.com>
2023-10-27 12:43:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ TOKEN = getenv("TOKEN", "6577602955:AAHpVhSSau9jV7K1fPFVDa2io5F7eMfRlqk")
 
 MONGO_DB_URL = getenv(
     "MONGO_DB_URL",
-    "mongodb+srv://karthika:karthika@cluster0.6jms6m3.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://monivps5:monivps5@cluster0.kmbq8we.mongodb.net/?retryWrites=true&w=majority",
 )
