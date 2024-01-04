652e9e1f117a75bf514d406932d78e872e7e3b47
Akash <126413936+crazyworld-izzy@users.noreply.github.com>
2024-01-04 22:30:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ LOAD = getenv("LOAD", "").split()
 
 NO_LOAD = getenv("NO_LOAD", "").split()
 
-TOKEN = getenv("TOKEN", "6009759960:AAGA_Pshq9D3TvtQWf1pgkv0X_JC5WuDDzQ")
+TOKEN = getenv("TOKEN", "6009759960:AAEMOCsUrfzdqesHaXE214ERgqQr1sRzlhU")
 
 MONGO_DB_URL = getenv(
     "MONGO_DB_URL",
-    "mongodb+srv://moni:moni@cluster0.dcufenj.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://moni:<password>@cluster0.i3djv5s.mongodb.net/?retryWrites=true&w=majority",
 )
